f65004716ad300130fadb6e59ccac4f7163d1cb9
lucas william <lucaswilliam133@gmail.com>
2020-09-30 00:29:28 -0300

    add algoritms and exercice of graphs in neps

---
+++ Algoritmos/Euclides/euclides.cpp
@@ -0,0 +1,12 @@
+// Retorna o MDC de um numero por meio de recursao
+
+int mdc (int a, int b) {
+    if (b == 0) return a;
+    else return mdc(b, a % b); 
+}
+
+// Usando operador ternário
+
+int mdc (int a, int b) {
+    return (b == 0) ? a : mdc (b, a % b);
+}
---
+++ Algoritmos/Recursao/fatorial.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+	
+ll fatorial (ll num){
+	if (num == 0) return 1;
+	else return (num * fatorial(num-1));
+}	
+
+
+int main (){
+	ll num;
+	cin >> num;
+	cout << fatorial (num) << endl;	
+	return 0;
+}
