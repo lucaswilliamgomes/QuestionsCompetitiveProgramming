5c21ff745fb803b580b02646051ee40a8eee1c34
lucas william <lucaswilliam133@gmail.com>
2020-10-20 18:17:55 -0300

    add hackerRank/Minimum swaps

--- HackerRank/Minimum Swaps 2.cpp
+++ HackerRank/Minimum Swaps 2.cpp
@@ -1,67 +1,13 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-/*        TLE
-int minimumSwaps(vector<int> arr) {
-    int aux, ans = 0;
-    
-    int tam = arr.size();
-
-    for (int i = 0; i < tam; i++){
-        if (arr[i] != (i+1)){
-            for (int j = (i+1); j < tam; j++){
-                if ((i+1) == arr[j]) {
-                    aux = arr[i];
-                    //cout << "Trocando o " << arr[i] << " pelo " << arr[j] << endl; 
-                    arr[i] = arr[j];
-                    arr[j] = aux;
-                    ans++;
-                    break;
-                }
-            }
-        }
-    }
-    return ans;
-
-}*/
-
-
-
 
 int minimumSwaps(vector<int> arr) {
-    int tam = arr.size();
-    pair <int, int> elemPos[tam];    
-    int ans = 0;
-
-    for (int i = 0; i < tam; i++){
-        elemPos[i].first = arr[i];
-        elemPos[i].second = i+1;
-    }
+    int tam = arr.size(), ans = 0;
     
-    for (int i = 0; i < tam; i++){    
-                                                        // 1 2 3 4 5   2 1 3 4 5   2 3 1 4 5   1 2 4 1 5
-        if (elemPos[i].first != elemPos[i].second) {    // 2 3 4 1 5   2 3 4 1 5   2 3 4 1 5   2 3 4 1 5
-            
-            int aux1 = elemPos[i].first;
-            int aux2 = elemPos[i].second;
-
-            elemPos[i].second = aux1;
-
-            elemPos[aux2].second = aux1;
-
-
-            /*for (int j = i; j < tam; j++){
-                if (elemPos[i].first == elemPos[j].second){
-                    int aux = elemPos[i].second;
-                    elemPos[i].second = elemPos[j].second;
-                    elemPos[j].second = aux;
-                }
-            }*/
-
-            /*for (int j = 0; j < tam; j++){
-                cout << elemPos[j].first << " " << elemPos[j].second << endl;    
-            }*/
-
+    for (int i = 0; i < tam; i++){
+        while (arr[i] != (i+1)){
+            swap(arr[i], arr[arr[i] - 1]);
             ans++;
         }
     }
--- Neps/Interruptores - 412.cpp
+++ Neps/Interruptores - 412.cpp
@@ -2,57 +2,26 @@
 
 using namespace std;
 
-bool verifica (vector <bool> lampadas) {
-
-    for (bool it : lampadas){
-        if (!it) return false;
-    }
-
-    return true;
-}
+vector <int> interruptores[1020];
 
 int main () {
-    int totalint, totallamp;
-    cin >> totalint >> totallamp;
-
     
-    vector <int> interruptores[1020];
-    
-    int l, apertar, aux, aux2, ans = 0;
-    cin >> l;
+    cin >> inter >> lamp;
+    bool acesas [lamp + 5];
 
-    vector <bool> lampadas(totallamp);
-
-    for (int i = 0; i < l; i++){
-        cin >> aux;
-        lampadas[aux] = true;
+    for (int i = 1; i <= lamp; i++){
+        acesas[i] = false;
     }
 
-    for (int i = 1; i <= totalint; i++){
+    cout << verifica(acesas) << endl;
+    cin >> ligadas;
+
+    for (int i = 0; i < ligadas; i++){
         cin >> aux;
-        for (int j = 1; j <= aux; j++){
-            cin >> aux2;
-            interruptores[i].push_back(aux2);
-        }
+        acesas[aux] = true;
     }
 
-    //while (!verifica(lampadas)){
-        for (int i = 1; i <= totalint; i++){
-            for (int j = 0; j < interruptores[i].size(); j++){
 
-                apertar = interruptores[i][j];
-                if(lampadas[apertar] == true) lampadas[apertar] = false;
-                else lampadas[apertar] = false;
-                ans++;
 
-            }  
-            if (i == totalint) {
-                cout << ans << endl;
-                return 0;
-            }
-        }   
-    //}
-
-    cout << ans << endl;
     return 0;
 }
