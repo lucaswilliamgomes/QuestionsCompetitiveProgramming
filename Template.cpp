6b22fd2d858fff34c0da3fc747149495fde2bb1e
Lucas William <lucaswilliam133@gmail.com>
2022-10-07 00:12:59 -0300

    add macacario

--- Template.cpp
+++ Template.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define optimize                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL)
 #define INF 100000010
 #define ALL(x) x.begin(), x.end()
 #define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
-#define REP(i, a, b) for(int i = (a); i <= (b); i++)
+#define REP(i, a, b) for (int i = (a); i <= (b); i++)
 #define POS(n, x) (lower_bound(ALL(n), x) - (n).begin())
 #define ll long long
 #define ld long double
-#define pii pair<int,int>
+#define pii pair<int, int>
 #define vi vector<int>
 #define vii vector<pii>
 #define os_type int
 #define PB push_back
 #define EB emplace_back
 #define MOD 1000000007
-#define PRIME 101 
+#define PRIME 101
 #define MAXN 101010
 #define MAXL 23
 #define endl '\n'
@@ -24,12 +27,12 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-#define ordered_set tree<os_type, null_type,less<os_type>, rb_tree_tag,tree_order_statistics_node_update>
+#define ordered_set tree<os_type, null_type, less<os_type>, rb_tree_tag, tree_order_statistics_node_update>
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     optimize;
+    
 
     return 0;
 }
